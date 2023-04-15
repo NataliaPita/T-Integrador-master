973b20a7902c7699b8ec3412eaf77808b5d3b381
NataliaPita <PitaFraile@gmail.com>
2023-04-14 21:14:45 -0300

    agregamos contenido a la sección nosotros

---
+++ script.js
@@ -0,0 +1,15 @@
+let nosText_btn= document.getElementById ('nosText_btn');
+let nosText = document.getElementById('nosText');
+
+nosText_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    nosText.classList.toggle('mostrar')
+    if(nosText.classList.contains('mostrar')){
+        nosText_btn.innerHTML = 'Saber menos';        
+    }
+    else{
+        nosText_btn.innerHTML = 'Saber Más'
+    }
+}
+
